86fae7171e7e7980fa0fe7c85bcb86f689905f5c
sp1r1tcruh3r <seventiy@bk.ru>
2019-10-10 20:30:25 +0300

    ----

--- jsx_title_text.js
+++ jsx_title_text.js
@@ -28,3 +28,90 @@ export default ({ title, text }) => {
       </div>
     </div>
   );
+// VARIANT S PROPSAMI
+  import React from 'react';
+
+  // BEGIN
+  export default class Card extends React.Component {
+    render() {
+      const { title, text } = this.props;
+      return (
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{text}</p>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  //dl+dt
+import { uniqueId } from 'lodash';
+import React from 'react';
+
+// BEGIN (write your solution here)
+export default class Definitions extends React.Component {
+  render() {
+    const { data } = this.props;
+    if (data.length === 0) {
+      return null;
+    }
+
+    const tags = data.map(({ dd, dt }) => (
+      <React.Fragment key={uniqueId()}>
+        <dt>{dt}</dt>
+        <dd>{dd}</dd>
+      </React.Fragment>
+    ));
+
+    return (
+      <dl>
+        {tags}
+      </dl>
+    );
+  }
+}
+// END
+//progressbar
+export default class Progress extends React.Component {
+  render() {
+    const { percentage } = this.props;
+    return (
+      <div className="progress">
+        <div
+          className="progress-bar"
+          role="progressbar"
+          style={{ width: `${percentage}%` }}
+          aria-valuenow={percentage}
+          aria-valuemin="0"
+          aria-valuemax="100"
+        />
+      </div>
+    );
+  }
+}
+
+//CLASSES INLINE
+/* eslint-disable react/prefer-stateless-function */
+
+import cn from 'classnames';
+import React from 'react';
+
+// BEGIN
+export default class Alert extends React.Component {
+  render() {
+    const { type, text } = this.props;
+
+    const alertClass = cn({
+      alert: true,
+      [`alert-${type}`]: true,
+    });
+    return (
+      <div className={alertClass} role="alert">
+        {text}
+      </div>
+    );
+  }
+}
+// END
